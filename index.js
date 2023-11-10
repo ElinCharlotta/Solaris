10abbe83456d4c0c48b659d4cf52124b9a53a644
ElinCharlotta <janssonelin@hotmail.com>
2023-11-10 20:03:10 +0100

    added code
    trying things out

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const planetsElem = document.querySelector('.planets')
-
-
-const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
 //const API_KEY= 'solaris-1Cqgm3S6nlMechWO'
 
 /* async function getApiKey(){
@@ -12,31 +8,48 @@ const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
 getApiKey() */
 
 async function getSolarisPlanets() {
-  let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
-    method: "GET",
-    headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
-  });
-  const data = await resp.json()
-  console.log(data);
+  try 
+  {
+    let response = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+      method: "GET",
+      headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
+    });
 
-  let planets = data.bodies;
-  console.log(planets)
-  showPlanetsInfo(planets)
+    const data = await response.json()
+
+
+    let planets = data.bodies;
+    console.log(planets);
+
+    showPlanetsInfo(planets)
+  } catch (error) {
+    console.error('No planets found')
+  }
 }
 getSolarisPlanets();
 
-//dynamiskt -  loopa igenom - matcha ihop direkt med HTML
 
-//ersätter bilden med infotext.inte riktigt det vi vill göra
+//Tips från Christoffer dynamiskt -  loopa igenom - matcha ihop direkt med HTML
 function showPlanetsInfo(planets) {
+// note to self, hur ska vi få in solen i detta? den har ju ingen img-tagg
   planets.forEach(planet => {
-    const planetElem = document.querySelector(`.${planet.name.toLowerCase()}`);
-    if (planetElem) {
-      planetElem.innerHTML = `        
-      <p>ID: ${planet.id}</p>
-      <p>Name: ${planet.name}</p>
-      <p>Latin Name: ${planet.latinName}</p>`
+      const findPlanetsImage = document.querySelector(`.${planet.name.toLowerCase()} img`); 
+      console.log(findPlanetsImage);
 
-    }
+      if (findPlanetsImage) {
+        findPlanetsImage.addEventListener("click", () => {
+              console.log('clicked planet');
+              displayPlanetInfo(planet);
+          });
+      }
   });
 }
+
+
+//bara temporärt
+function displayPlanetInfo(planet) {
+  alert(`Planet Name: ${planet.name}\nPlanet Type: ${planet.type}\nPlanet temp: ${planet.temp.day}`);
+}
+
+
+
