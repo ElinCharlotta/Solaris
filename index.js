84d5c71748cfc35c72de9ef0a60dcba39e4dfc19
ElinCharlotta <janssonelin@hotmail.com>
2023-11-10 09:19:53 +0100

    making mistakes
    Working on code. Not pretty

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const planetsElem = document.querySelector('.planets')
+
+
 const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
 //const API_KEY= 'solaris-1Cqgm3S6nlMechWO'
 
@@ -8,19 +11,32 @@ const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
 }
 getApiKey() */
 
-async function getSolarisBodies() {
+async function getSolarisPlanets() {
   let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
     method: "GET",
     headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
   });
   const data = await resp.json()
-  console.log(data)
+  console.log(data);
 
+  let planets = data.bodies;
+  console.log(planets)
+  showPlanetsInfo(planets)
 }
-getSolarisBodies();
+getSolarisPlanets();
 
+//dynamiskt -  loopa igenom - matcha ihop direkt med HTML
 
+//ersätter bilden med infotext.inte riktigt det vi vill göra
+function showPlanetsInfo(planets) {
+  planets.forEach(planet => {
+    const planetElem = document.querySelector(`.${planet.name.toLowerCase()}`);
+    if (planetElem) {
+      planetElem.innerHTML = `        
+      <p>ID: ${planet.id}</p>
+      <p>Name: ${planet.name}</p>
+      <p>Latin Name: ${planet.latinName}</p>`
 
-
-
-
+    }
+  });
+}
