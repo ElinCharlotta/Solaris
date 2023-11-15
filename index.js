0894dc7d4dc2949a2930c03319396121edb6bb67
ElinCharlotta <janssonelin@hotmail.com>
2023-11-15 18:33:36 +0100

    added code
    added code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 bodyElem = document.querySelector('body');
 //const API_KEY= 'solaris-1Cqgm3S6nlMechWO'
 
-/* async function getApiKey(){
+/*async function getApiKey() {
   let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
     method: "POST"
   });
-}
-getApiKey() */
+  const data = await resp.json();
+  console.log(data);
+  getSolarisPlanets(data.apiKey)
+}catch (error){console.error('No key found)}
+getApiKey()*/
 
 async function getSolarisPlanets() {
   try {
@@ -19,7 +22,7 @@ async function getSolarisPlanets() {
 
     let planets = data.bodies;
     console.log(planets)
-    showPlanetsInfo(planets)
+    attachPlanetClick(planets)
   } catch (error) {
     console.error('No planets found')
   }
@@ -27,13 +30,13 @@ async function getSolarisPlanets() {
 getSolarisPlanets();
 
 
-function showPlanetsInfo(planets) {
-
+function attachPlanetClick(planets) {
+  //go through each planet one at a time and add a 'click' to each planet. So I don't have to add click event to all planets separately.
   planets.forEach(planet => {
-    const getPlanetsInfo = document.querySelector(`.${planet.name.toLowerCase()}`);
+    const planetElem = document.querySelector(`.${planet.name.toLowerCase()}`);
 
-    if (getPlanetsInfo) {
-      getPlanetsInfo.addEventListener('click', () => {
+    if (planetElem) {
+      planetElem.addEventListener('click', () => {
 
         displayPlanetInfo(planet);
       });
@@ -41,33 +44,60 @@ function showPlanetsInfo(planets) {
   });
 }
 
+
 function displayPlanetInfo(planet) {
   const infoPage = document.createElement('div');
   infoPage.classList.add('planet-info-page');
 
+  let planetsMoonInfo = '';
+  let planetDistanceInfo = '';
+  let temperatureSection = '';
+
+  if (planet.moons.length > 0) {
+    planetsMoonInfo = `<p class='planet-moon'>MÅNAR<br>${planet.moons.join(',  ').split(' ').join('&nbsp;')}</p>`;
+  }
+
+  if (planet.distance > 0) {
+    planetDistanceInfo = `<p class='planet-distance'>KM FRÅN SOLEN<br>${planet.distance}</p>`;
+  }
+
+  if (planet.name.toLowerCase() === 'solen') {
+
+    temperatureSection = `
+  <p class='planet-temp-day'>LÄGSTA TEMPERATUR <br>${planet.temp.night}</p>
+  <p class='planet-temp-night'>HÖGSTA TEMPERATUR <br>${planet.temp.day}</p>`;
+    infoPage.classList.add('info-solen')
+  }
+  else {
+    temperatureSection = `
+  <p class='planet-temp-day'>LÄGSTA TEMPERATUR <br>${planet.temp.night}</p>
+  <p class='planet-temp-night'>HÖGSTA TEMPERATUR <br>${planet.temp.day}</p>`
+
+  }
+
   infoPage.innerHTML = `
-  <main class='planets-info'>
-  <button class="close-button" onclick="closePlanetInfo()">&times;</button>
+    <main class='planets-info'>
+      <button class="close-button" onclick="closePlanetInfo()">&times;</button>
 
-<h1 class='planet-name' style='text-transform: uppercase;'>${planet.name}</h1>
-<h2 class='planet-latin-name' style='text-transform: uppercase;'> ${planet.latinName}</h2>
+      <h1 class='planet-name' style='text-transform: uppercase;'>${planet.name}</h1>
+      <h2 class='planet-latin-name' style='text-transform: uppercase;'> ${planet.latinName}</h2>
 
-<p class='planet-description'> ${planet.desc}</p>
+      <p class='planet-description'> ${planet.desc}</p>
 
-<article class="quick-info">
-<p class='planet-circumference'>OMKRETS<br> ${planet.circumference}</p>
-<p class='planet-distance'>KM FRÅN SOLEN<br> ${planet.distance}</p>
-<p class='planet-temp-day'>MINSTA TEMPERATUR <br> ${planet.temp.night}</p>
-<p class='planet-temp-night'>HÖGSTA TEMPERATUR <br> ${planet.temp.day}</p>
-</article>
-<p class='planet-moon'>MÅNAR<br>${planet.moons.join(',  ').split(' ').join('&nbsp;')}</p>
-</main>
+      <article class="quick-info">
+        <p class='planet-circumference'>OMKRETS<br>${planet.circumference}</p>
+        ${planetDistanceInfo}
+     
+        ${temperatureSection}
+      </article>
 
-`;
-  console.log(planet.moons)
+      ${planetsMoonInfo}
+    </main>
+  `;
+
   bodyElem.appendChild(infoPage);
-
 }
+
 
 function closePlanetInfo() {
   const infoPage = document.querySelector('.planet-info-page');
